e14bdf0d95eae3cd8547ff28ab9c05a2a0c65935
Julian_Chu <walkingice0204@gmail.com>
2021-07-22 22:43:09 +0900

    build: update appcompat to 1.3.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val android_gradle_plugin = "4.2.1"
     const val gms_oss_licenses_plugin = "0.10.2"
     const val support = "1.0.0"
-    const val appcompat = "1.1.0-rc01"
+    const val appcompat = "1.3.1"
     const val material = "1.1.0-alpha05"
     const val cardview = "1.0.0"
     const val recyclerview = "1.1.0"

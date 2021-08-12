3f1488356b5cb82b9557a98c3f072c61aed5e184
Julian_Chu <walkingice0204@gmail.com>
2021-08-12 23:36:23 +0900

    private: PrivateHomeFragment use view binding

--- app/src/main/java/org/mozilla/rocket/privately/home/PrivateHomeFragment.kt
+++ app/src/main/java/org/mozilla/rocket/privately/home/PrivateHomeFragment.kt
@@ -21,9 +21,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
-import com.airbnb.lottie.LottieAnimationView
 import dagger.Lazy
 import org.mozilla.focus.R
+import org.mozilla.focus.databinding.FragmentPrivateHomescreenBinding
 import org.mozilla.focus.locale.LocaleAwareFragment
 import org.mozilla.focus.navigation.ScreenNavigator
 import org.mozilla.focus.telemetry.TelemetryWrapper
@@ -40,13 +40,12 @@ class PrivateHomeFragment :
     LocaleAwareFragment(),
     ScreenNavigator.HomeScreen {
 
+    var binding: FragmentPrivateHomescreenBinding? = null
+
     @Inject
     lateinit var chromeViewModelCreator: Lazy<ChromeViewModel>
 
     private lateinit var chromeViewModel: ChromeViewModel
-    private lateinit var logoMan: LottieAnimationView
-    private lateinit var fakeInput: View
-    private lateinit var privateModeBtn: View
 
     @Override
     override fun onCreate(bundle: Bundle?) {
@@ -56,24 +55,22 @@ class PrivateHomeFragment :
     }
 
     @Override
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, state: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_private_homescreen, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentPrivateHomescreenBinding.inflate(inflater, container, false).also {
+        this.binding = it
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        logoMan = view.findViewById(R.id.pm_home_logo)
-        fakeInput = view.findViewById(R.id.pm_home_fake_input)
         initDescription(view)
 
-        fakeInput.setOnClickListener { chromeViewModel.showUrlInput.call() }
-        chromeViewModel.isHomePageUrlInputShowing.observe(
-            viewLifecycleOwner,
-            Observer { isShowing ->
-                if (isShowing == true) hideFakeInput() else showFakeInput()
-            }
-        )
-        privateModeBtn = view.findViewById(R.id.pm_home_private_mode_btn)
-        privateModeBtn.setOnClickListener {
+        val binding = this.binding ?: return
+
+        binding.pmHomeFakeInput.setOnClickListener { chromeViewModel.showUrlInput.call() }
+        binding.pmHomePrivateModeBtn.setOnClickListener {
             chromeViewModel.togglePrivateMode.call()
             TelemetryWrapper.togglePrivateMode(false)
         }
@@ -89,7 +86,12 @@ class PrivateHomeFragment :
         val descriptionStr = getString(R.string.private_home_description_1, highlightStr)
         val str = SpannableString(descriptionStr).apply {
             val highlightIndex = descriptionStr.indexOf(highlightStr)
-            setSpan(ForegroundColorSpan(Color.WHITE), highlightIndex, highlightIndex + highlightStr.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            setSpan(
+                ForegroundColorSpan(Color.WHITE),
+                highlightIndex,
+                highlightIndex + highlightStr.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
         }
         inflatedView.findViewById<TextView>(R.id.pm_home_brand_description).text = str
     }
@@ -101,30 +103,36 @@ class PrivateHomeFragment :
             monitorShortcutMessage(this, shortcutViewModel)
             monitorShortcutCreation(this, shortcutViewModel)
         }
+
+        chromeViewModel.isHomePageUrlInputShowing.observe(viewLifecycleOwner) { isShowing ->
+            if (isShowing == true) hideFakeInput() else showFakeInput()
+        }
     }
 
     private fun monitorShortcutPromotion(context: Context, model: ShortcutViewModel) {
-        model.eventPromoteShortcut.observe(
-            viewLifecycleOwner,
-            Observer { callback ->
-                val data = CustomViewDialogData().apply {
-                    this.drawable = VectorDrawableCompat.create(resources, R.drawable.dialog_pbshortcut, null)
-                    this.title = context.getString(R.string.private_browsing_dialog_add_shortcut_title_v2)
-                    this.description = context.getString(R.string.private_browsing_dialog_add_shortcut_content_v2)
-                    this.positiveText = context.getString(R.string.private_browsing_dialog_add_shortcut_yes_v2)
-                    this.negativeText = context.getString(R.string.private_browsing_dialog_add_shortcut_no_v2)
-                    this.showCloseButton = true
-                }
-
-                PromotionDialog(context, data)
-                    .onPositive { callback?.onPositive() }
-                    .onNegative { callback?.onNegative() }
-                    .onClose { callback?.onNegative() }
-                    .onCancel { callback?.onCancel() }
-                    .setCancellable(false)
-                    .show()
+        model.eventPromoteShortcut.observe(viewLifecycleOwner) { callback ->
+            val data = CustomViewDialogData().apply {
+                this.drawable =
+                    VectorDrawableCompat.create(resources, R.drawable.dialog_pbshortcut, null)
+                this.title =
+                    context.getString(R.string.private_browsing_dialog_add_shortcut_title_v2)
+                this.description =
+                    context.getString(R.string.private_browsing_dialog_add_shortcut_content_v2)
+                this.positiveText =
+                    context.getString(R.string.private_browsing_dialog_add_shortcut_yes_v2)
+                this.negativeText =
+                    context.getString(R.string.private_browsing_dialog_add_shortcut_no_v2)
+                this.showCloseButton = true
             }
-        )
+
+            PromotionDialog(context, data)
+                .onPositive { callback?.onPositive() }
+                .onNegative { callback?.onNegative() }
+                .onClose { callback?.onNegative() }
+                .onCancel { callback?.onCancel() }
+                .setCancellable(false)
+                .show()
+        }
     }
 
     private fun monitorShortcutMessage(context: Context, model: ShortcutViewModel) {
@@ -157,10 +165,11 @@ class PrivateHomeFragment :
     }
 
     private fun animatePrivateHome() {
-        privateModeBtn.apply {
-            findViewById<LottieAnimationView>(R.id.pm_home_mask).playAnimation()
+        val binding = this.binding ?: return
+        binding.pmHomePrivateModeBtn.apply {
+            binding.pmHomeMask.playAnimation()
         }
-        logoMan.playAnimation()
+        binding.pmHomeLogo.playAnimation()
     }
 
     override fun onUrlInputScreenVisible(visible: Boolean) {
@@ -172,13 +181,13 @@ class PrivateHomeFragment :
     }
 
     private fun showFakeInput() {
-        logoMan.visibility = View.VISIBLE
-        fakeInput.visibility = View.VISIBLE
+        binding?.pmHomeLogo?.visibility = View.VISIBLE
+        binding?.pmHomeFakeInput?.visibility = View.VISIBLE
     }
 
     private fun hideFakeInput() {
-        logoMan.visibility = View.INVISIBLE
-        fakeInput.visibility = View.INVISIBLE
+        binding?.pmHomeLogo?.visibility = View.INVISIBLE
+        binding?.pmHomeFakeInput?.visibility = View.INVISIBLE
     }
 
     override fun onCreateAnimation(transit: Int, enter: Boolean, nextAnim: Int): Animation? {

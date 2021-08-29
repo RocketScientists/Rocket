d9fd05516a12a6889211ad93de14963fb055b415
Julian_Chu <walkingice0204@gmail.com>
2021-08-29 16:53:01 +0900

    private: copy TrackerPopup implementation to Menu Dialog
    Now, even in Private Mode, we could still block trackers, and the Dialog
    has buttons for page back/foward.

--- app/src/main/java/org/mozilla/rocket/menu/PrivateBrowserMenuDialog.kt
+++ app/src/main/java/org/mozilla/rocket/menu/PrivateBrowserMenuDialog.kt
@@ -8,13 +8,19 @@ import android.os.Looper
 import android.view.View
 import android.view.ViewOutlineProvider
 import androidx.annotation.StyleRes
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import dagger.Lazy
+import mozilla.components.concept.engine.EngineSession
+import org.mozilla.focus.FocusApplication
 import org.mozilla.focus.R
 import org.mozilla.focus.databinding.BottomSheetPrivateBrowserMenuBinding
+import org.mozilla.focus.web.BrowsingSession
 import org.mozilla.rocket.chrome.BottomBarItemAdapter
 import org.mozilla.rocket.chrome.ChromeViewModel
 import org.mozilla.rocket.chrome.PrivateMenuViewModel
 import org.mozilla.rocket.chrome.bottombar.BottomBarItem.ItemType
+import org.mozilla.rocket.content.app
 import org.mozilla.rocket.content.appComponent
 import org.mozilla.rocket.content.getActivityViewModel
 import org.mozilla.rocket.extension.nonNullObserve
@@ -36,6 +42,9 @@ class PrivateBrowserMenuDialog : LifecycleBottomSheetDialog {
 
     private lateinit var binding: BottomSheetPrivateBrowserMenuBinding
     private val uiHandler = Handler(Looper.getMainLooper())
+
+    private val counterColorEnabled = ContextCompat.getColor(context, R.color.palettePurple100)
+    private val counterColorDisabled = ContextCompat.getColor(context, R.color.paletteWhite100)
 
     constructor(context: Context) : super(context)
     constructor(context: Context, @StyleRes theme: Int) : super(context, theme)
@@ -70,8 +79,8 @@ class PrivateBrowserMenuDialog : LifecycleBottomSheetDialog {
         setContentView(binding.root)
 
         initBottomBar()
+        initTrackerView()
     }
-
 
     private fun initBottomBar() {
         val bottomBar = binding.menuBottomBar
@@ -108,5 +117,82 @@ class PrivateBrowserMenuDialog : LifecycleBottomSheetDialog {
         chromeViewModel.canGoBack.switchFrom(menuViewModel.bottomItems).observe(this) {
             bottomBarItemAdapter.setCanGoBack(it == true)
         }
+    }
+
+    private fun initTrackerView() {
+        binding.trackerContentView.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                context,
+                R.drawable.background_tracker_popup
+            )
+        )
+
+        val switchView = binding.trackerSwitch
+        DrawableCompat.setTintList(
+            switchView.thumbDrawable,
+            ContextCompat.getColorStateList(context, R.color.switch_thumb_dark)
+        )
+        DrawableCompat.setTintList(
+            switchView.trackDrawable,
+            ContextCompat.getColorStateList(context, R.color.switch_track_dark)
+        )
+
+        val counterBkg = DrawableCompat.wrap(binding.trackerCountContainer.background).mutate()
+        binding.trackerCountContainer.background = counterBkg
+
+        val counterView = binding.trackerCount
+        switchView.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                counterView.text = "0"
+                DrawableCompat.setTint(counterBkg, counterColorEnabled)
+            } else {
+                binding.trackerCount.text = COUNT_DISABLED
+                DrawableCompat.setTint(counterBkg, counterColorDisabled)
+            }
+
+            setTurboModeSettingInPrivateMode(isChecked)
+        }
+
+        val isTurboModeEnabled = isTurboModeEnabledInPrivateMode(context)
+        binding.trackerSwitch.isChecked = isTurboModeEnabled
+        BrowsingSession.getInstance().blockedTrackerCount.observe(this) { setBlockedCount(it) }
+    }
+
+    private fun setBlockedCount(count: Int?) {
+        if (!binding.trackerSwitch.isChecked) {
+            return
+        }
+
+        val countStr = count?.toString() ?: return
+        binding.trackerCount.text = if (countStr.length <= MAX_NUMBER_OF_DIGITS) {
+            countStr
+        } else {
+            COUNT_INFINITY
+        }
+    }
+
+    private fun isTurboModeEnabledInPrivateMode(context: Context): Boolean {
+        val appContext = context.applicationContext as? FocusApplication ?: return false
+        return appContext.settings.privateBrowsingSettings.shouldUseTurboMode()
+    }
+
+    private fun setTurboModeSettingInPrivateMode(isEnabled: Boolean) {
+        chromeViewModel.isPrivateTurboModeEnabled.value = isEnabled
+        val policy = if (isEnabled) {
+            EngineSession.TrackingProtectionPolicy.all()
+        } else {
+            null
+        }
+        app().settings.privateBrowsingSettings.setTurboMode(isEnabled)
+        app().engineSettings.trackingProtectionPolicy = policy
+        // TODO: move to Session.Observer.onTrackerBlockingEnabledChanged
+        // for now the callback has a bug in version 0.52.0
+        bottomBarItemAdapter.setTrackerSwitch(isEnabled)
+    }
+
+    companion object {
+        private const val COUNT_INFINITY = "∞"
+        private const val COUNT_DISABLED = "-"
+        private const val MAX_NUMBER_OF_DIGITS = 2
     }
 }

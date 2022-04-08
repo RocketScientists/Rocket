2ab1eee471c41c81cc479b636fec49a60480b8e7
Julian_Chu <walkingice0204@gmail.com>
2022-04-08 23:22:13 +0900

    migrate AboutPreference

--- app/src/main/java/org/mozilla/rocket/settings/AboutPreference.kt
+++ app/src/main/java/org/mozilla/rocket/settings/AboutPreference.kt
@@ -2,15 +2,16 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-package org.mozilla.focus.widget
+package org.mozilla.rocket.settings
 
 import android.content.Context
-import android.preference.Preference
 import android.util.AttributeSet
+import androidx.preference.Preference
 import org.mozilla.focus.R
 
 class AboutPreference @JvmOverloads constructor(
-    context: Context?, attrs: AttributeSet?,
+    context: Context?,
+    attrs: AttributeSet?,
     defStyleAttr: Int = 0
 ) : Preference(context, attrs, defStyleAttr) {
     init {

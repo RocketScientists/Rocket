f3c47b9ce13bf340fc9e3b8328360e0b62f1b8eb
Julian_Chu <walkingice0204@gmail.com>
2022-04-08 23:21:38 +0900

    convert AboutPreference from Java to Kotlin

---
+++ app/src/main/java/org/mozilla/focus/widget/AboutPreference.kt
@@ -0,0 +1,20 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.focus.widget
+
+import android.content.Context
+import android.preference.Preference
+import android.util.AttributeSet
+import org.mozilla.focus.R
+
+class AboutPreference @JvmOverloads constructor(
+    context: Context?, attrs: AttributeSet?,
+    defStyleAttr: Int = 0
+) : Preference(context, attrs, defStyleAttr) {
+    init {
+        val title = getContext().resources.getString(R.string.preference_about)
+        setTitle(title)
+    }
+}

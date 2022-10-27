e6dc7caeb188545f5416ce4d4649100771d34eaa
Julian_Chu <walkingice0204@gmail.com>
2022-10-27 22:33:07 +0900

    remove Shop classes

--- app/src/main/java/org/mozilla/rocket/shopping/search/domain/GetSearchPromptMessageShowCountUseCase.kt
+++
@@ -1,9 +0,0 @@
-package org.mozilla.rocket.shopping.search.domain
-
-import org.mozilla.rocket.shopping.search.data.ShoppingSearchRepository
-
-class GetSearchPromptMessageShowCountUseCase(val repository: ShoppingSearchRepository) {
-    operator fun invoke(): Int {
-        return repository.getSearchPromptMessageShowCount()
-    }
-}
